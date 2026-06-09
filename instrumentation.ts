4245620a2f74bdc19d4adfe4c94db93a527eb49b
fogyisland <10853913@qq.com>
2026-06-09 21:00:33 +0800

    feat(server): run initDb on server startup (Plan B)

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,8 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
+    const { initDb } = await import('./scripts/init-db');
     const { loadDictionaries } = await import('./server/dictionary');
+    await initDb();
     loadDictionaries();
   }
 }
